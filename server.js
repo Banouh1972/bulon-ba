c0a803d770818a046b775f957ea19baf3a430260
Banouh1972 <coulibn1@gmail.com>
2026-04-21 11:34:29 +0000

    Integrate WebSocket server into HTTP server
    Added WebSocket server functionality and updated response message.

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const WebSocket = require('ws');
 
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('OK');
+  res.end('OK - Server running');
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log('Message:', message.toString());
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
 });
 
 server.listen(port, '0.0.0.0', () => {
-  console.log(`HTTP server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
